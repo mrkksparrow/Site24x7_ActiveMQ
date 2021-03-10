9633bdecc7dba95c910dfec3904358ade74f6a2e
Kavin Kumar <40456399+mrkksparrow@users.noreply.github.com>
2021-03-10 22:53:20 +0530

    Update activemq.py

--- activemq.py
+++ activemq.py
@@ -5,16 +5,27 @@ import argparse
 from jmxquery import JMXConnection
 from jmxquery import JMXQuery
 
-
+# if any impacting changes to this plugin kindly increment the plugin version here.
 PLUGIN_VERSION = 1
+
+# Setting this to true will alert you when there is a communication problem while posting plugin data to server
 HEARTBEAT = "true"
 
+# Enter the host name configures for the Solr JMX
 HOST_NAME = ""
+
+# Enter the port configures for the Solr JMX
 PORT = ""
+
+# Enter the Broker name to be monitored from your Apache Solr
+BROKER_NAME = ""
+
+# Enter the Destination name to be monitored from your Apache Solr
+DESTINATION_NAME = ""
+
 URL = ""
 QUERY = ""
-BROKER_NAME = ""
-DESTINATION_NAME = ""
+
 result_json = {}
 
 METRIC_UNITS = {
@@ -52,6 +63,7 @@ metric_map = {
 }
 
 
+# JMX Query is executed and getting the Performance metric data after filtering the output
 def mbean_attributes(jmxConnection, OUERY):
     try:
         jmxQuery = [JMXQuery(OUERY)]
@@ -72,6 +84,7 @@ def mbean_attributes(jmxConnection, OUERY):
     return result_json
 
 
+# JMX url is defined and JMX connection is established, Query and metric keys are passed to process
 def get_output():
     URL = "service:jmx:rmi:///jndi/rmi://" + HOST_NAME + ":" + PORT + "/jmxrmi"
     try:
@@ -93,13 +106,14 @@ def get_output():
     return result_json
 
 
+# arguments are parsed from activemq.cfg file and assigned with the variables
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('--host_name', help="squid host_name", type=str)
-    parser.add_argument('--port', help="squid port", type=str)
-    parser.add_argument('--broker_name', help="squid port", type=str)
-    parser.add_argument('--destination_name', help="squid host_name", type=str)
+    parser.add_argument('--host_name', help="activemq host_name", type=str)
+    parser.add_argument('--port', help="activemq port", type=str)
+    parser.add_argument('--broker_name', help="activemq broker_name", type=str)
+    parser.add_argument('--destination_name', help="activemq destination_name", type=str)
     args = parser.parse_args()
     if args.host_name:
         HOST_NAME = args.host_name
